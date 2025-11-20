d86ddb06924d188cf35f02adb33dcc4824bd0d11
ratandeora1 <ratansingh.deora@trooinbound.com>
2025-11-20 15:54:15 +0530

    Deal Code

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 
 // HubSpot API URL
 const HUBSPOT_URL = "https://api.hubapi.com/crm/v3/objects/contacts";
+const HUBSPOT_DEAL_URL = "https://api.hubapi.com/crm/v3/objects/deals";
 
 app.get('/get-contacts', async (req, res) => {
     try {
@@ -25,6 +26,27 @@ app.get('/get-contacts', async (req, res) => {
     } catch (error) {
         console.error("Error:", error.response?.data || error.message);
 
+        res.json({
+            success: false,
+            error: error.response?.data || error.message
+        });
+    }
+});
+app.get('/get-deals', async (req, res) => {
+    try {
+        const response = await axios.get(HUBSPOT_DEAL_URL, {
+            headers: {
+                Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`
+            }
+        });
+
+        res.json({
+            success: true,
+            total: response.data.total,
+            results: response.data.results
+        });
+
+    } catch (error) {
         res.json({
             success: false,
             error: error.response?.data || error.message
